54df5b5b8497ad45ae757e7b101f4cae717e29d6
Daniel Owens <DanielOwens@Daniel-Owenss-MacBook-Pro-2.local>
2019-06-25 15:29:34 -0700

    Updates

---
+++ precache-manifest.97785a3344fa84ad2da135e52d45bf89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8c558051a69d9f416ed",
+    "url": "/react-video-search/static/js/runtime~main.25cab1b0.js"
+  },
+  {
+    "revision": "5a649aeca0e1389e8461",
+    "url": "/react-video-search/static/js/main.3fbeb77d.chunk.js"
+  },
+  {
+    "revision": "5ebd480e7c8d00d3800c",
+    "url": "/react-video-search/static/js/2.177621ec.chunk.js"
+  },
+  {
+    "revision": "5a649aeca0e1389e8461",
+    "url": "/react-video-search/static/css/main.dcbe3534.chunk.css"
+  },
+  {
+    "revision": "ebbf9375069f25bd27c5417cefd30e8a",
+    "url": "/react-video-search/index.html"
+  }
+];
